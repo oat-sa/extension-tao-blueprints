258b0bede7467e6a04b842514966fdbaa6ca9e29
cmoyon <camille@taotesting.com>
2016-11-22 11:24:22 +0100

    Create empty version

---
+++ manifest.php
@@ -0,0 +1,59 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *               
+ * 
+ */
+
+/**
+ * Generated using taoDevTools 2.15.0
+ */
+return array(
+    'name' => 'taoBlueprints',
+    'label' => 'Blueprints extension',
+    'description' => 'Extension to manage blue prints',
+    'license' => 'GPL-2.0',
+    'version' => '0.0.1',
+    'author' => 'Open Assessment Technologies SA',
+    'requires' => array(
+        'tao' => '>=7.29.0'
+    ),
+    'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoBlueprintsManager',
+    'acl' => array(
+        array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoBlueprintsManager', array('ext'=>'taoBlueprints')),
+    ),
+    'install' => array(
+    ),
+    'uninstall' => array(
+    ),
+    'routes' => array(
+        '/taoBlueprints' => 'oat\\taoBlueprints\\controller'
+    ),    
+    'constants' => array(
+        # views directory
+        "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+        
+        #BASE URL (usually the domain root)
+        'BASE_URL' => ROOT_URL.'taoBlueprints/',
+        
+        #BASE WWW required by JS
+        'BASE_WWW' => ROOT_URL.'taoBlueprints/views/'
+    ),
+    'extra' => array(
+        'structures' => dirname(__FILE__).DIRECTORY_SEPARATOR.'controller'.DIRECTORY_SEPARATOR.'structures.xml',
+    )
+);
