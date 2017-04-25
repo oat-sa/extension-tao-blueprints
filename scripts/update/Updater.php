8c2f57de55e46756c66bcff8c299c479af12d9b0
siwane <camille@taotesting.com>
2017-04-25 08:32:04 +0000

    Unallow to delete a blueprint in usage into a test section

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -55,6 +55,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('0.4.0');
         }
 
-        $this->skip('0.4.0', '0.6.1');
+        $this->skip('0.4.0', '0.6.2');
     }
 }
